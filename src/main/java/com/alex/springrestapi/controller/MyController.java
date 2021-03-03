cd3f5702a7bb21b189289025dc832696c37bbc68
alex3195 <alex9954161@mail.ru>
2021-03-03 23:29:46 +0500

    spring rest api versio 0.0.1

---
+++ src/main/java/com/alex/springrestapi/controller/MyController.java
@@ -0,0 +1,41 @@
+package com.alex.springrestapi.controller;
+
+import com.alex.springrestapi.entity.Book;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+    private static List<Book> books = new ArrayList<>();
+
+    @PostMapping(value = {"/add_book"})
+    public List<Book> addBook(@RequestBody Book book) {
+        books.add(book);
+        return books;
+    }
+
+    @GetMapping(value = "/get_books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @GetMapping(value = {"/get_book_by_index/{id}"})
+    public Book getBookByIndex(@PathVariable("id") int id) {
+        return books.get(id);
+    }
+
+    @PutMapping(value = {"/edit_book/{id}"})
+    public List<Book> editBook(@PathVariable("id") int id, @RequestBody Book book) {
+        books.get(id).setName(book.getName());
+        return books;
+    }
+
+    @DeleteMapping(value = {"/delete_by_index/{id}"})
+    public List<Book> deleteByIndex(@PathVariable("id") int index) {
+        books.remove(index);
+        return books;
+    }
+}
